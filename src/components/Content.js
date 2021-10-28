2499bb1e8f924f7e5fce7f211a8915fb9ba34e7a
kitou88C39 <kanfutrooper@icloud.com>
2021-10-28 13:16:53 +0900

    課題② -1提出2回目

--- src/components/Content.js
+++ src/components/Content.js
@@ -79,38 +79,11 @@ const StepContent = ({ stepIndex, questionnaireProps }) => {
               <Select native defaultValue="" id="grouped-native-select" label="Grouping">
                 <option aria-label="None" value="" />
                 <optgroup label="year">
-                  <option value={1}>　1990</option>
-                  <option value={2}>　1991</option>
-                  <option value={3}>　1992</option>
-                  <option value={4}>　1993</option>
-                  <option value={5}>　1994</option>
-                  <option value={6}>　1995</option>
-                  <option value={7}>　1996</option>
-                  <option value={8}>　1997</option>
-                  <option value={9}>　1998</option>
-                  <option value={10}>　1999</option>
-                  <option value={11}>　2000</option>
-                  <option value={12}>　2001</option>
-                  <option value={13}>　2002</option>
-                  <option value={14}>　2003</option>
-                  <option value={15}>　2004</option>
-                  <option value={16}>　2005</option>
-                  <option value={17}>　2006</option>
-                  <option value={18}>　2007</option>
-                  <option value={19}>　2008</option>
-                  <option value={20}>　2009</option>
-                  <option value={21}>　2010</option>
-                  <option value={22}>　2011</option>
-                  <option value={23}>　2012</option>
-                  <option value={24}>　2013</option>
-                  <option value={25}>　2014</option>
-                  <option value={26}>　2015</option>
-                  <option value={27}>　2016</option>
-                  <option value={28}>　2017</option>
-                  <option value={29}>　2018</option>
-                  <option value={30}>　2019</option>
-                  <option value={31}>　2020</option>
-                  <option value={32}>　2021</option>
+                  {Array.from(Array(2020), (_, num) => (
+                    <option key={num} value={num + 1}>
+                      {num + 1}
+                    </option>
+                  ))}
                 </optgroup>
               </Select>
             </FormControl>
@@ -119,18 +92,11 @@ const StepContent = ({ stepIndex, questionnaireProps }) => {
               <Select native defaultValue="" id="grouped-native-select" label="Grouping">
                 <option aria-label="None" value="" />
                 <optgroup label="month">
-                  <option value={1}> 1</option>
-                  <option value={2}> 2</option>
-                  <option value={3}> 3</option>
-                  <option value={4}> 4</option>
-                  <option value={5}> 5</option>
-                  <option value={6}> 6</option>
-                  <option value={7}> 7</option>
-                  <option value={8}> 8</option>
-                  <option value={9}> 9</option>
-                  <option value={10}> 10</option>
-                  <option value={11}> 11</option>
-                  <option value={12}> 12</option>
+                  {Array.from(Array(12), (_, num) => (
+                    <option key={num} value={num + 1}>
+                      {num + 1}
+                    </option>
+                  ))}
                 </optgroup>
               </Select>
             </FormControl>
@@ -139,37 +105,11 @@ const StepContent = ({ stepIndex, questionnaireProps }) => {
               <Select native defaultValue="" id="grouped-native-select" label="Grouping">
                 <option aria-label="None" value="" />
                 <optgroup label="day">
-                  <option value={1}> 1</option>
-                  <option value={2}> 2</option>
-                  <option value={3}> 3</option>
-                  <option value={4}> 4</option>
-                  <option value={5}> 2</option>
-                  <option value={6}> 6</option>
-                  <option value={7}> 7</option>
-                  <option value={8}> 8</option>
-                  <option value={9}> 9</option>
-                  <option value={10}> 10</option>
-                  <option value={11}> 11</option>
-                  <option value={12}> 12</option>
-                  <option value={13}> 13</option>
-                  <option value={14}> 14</option>
-                  <option value={15}> 15</option>
-                  <option value={16}> 16</option>
-                  <option value={17}> 17</option>
-                  <option value={18}> 18</option>
-                  <option value={19}> 19</option>
-                  <option value={20}> 20</option>
-                  <option value={21}> 21</option>
-                  <option value={22}> 22</option>
-                  <option value={23}> 23</option>
-                  <option value={24}> 24</option>
-                  <option value={25}> 25</option>
-                  <option value={26}> 26</option>
-                  <option value={27}> 27</option>
-                  <option value={28}> 28</option>
-                  <option value={29}> 29</option>
-                  <option value={30}> 30</option>
-                  <option value={31}> 31</option>
+                  {Array.from(Array(12), (_, num) => (
+                    <option key={num} value={num + 1}>
+                      {num + 1}
+                    </option>
+                  ))}
                 </optgroup>
               </Select>
             </FormControl>
