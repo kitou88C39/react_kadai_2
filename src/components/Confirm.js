b63e9f49f12e8e844f966986d2ca6b9216e5c671
kitou88C39 <kanfutrooper@icloud.com>
2021-11-09 14:47:49 +0900

    React課題② -2質問2回目

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -1,11 +1,25 @@
 import React from "react";
+import Basic from "./Basic";
+import Questionnaire, { QUESTIONS } from "./Questionnaire";
+import Optional from "./Optional";
 
-const Confirm = () => {
+export const UserInputData = React.createContext();
+
+function Confirm() {
+  const [currentState, setCurrentState] = React.useState({});
+  const [answers, setAnswers] = React.useState(Array(QUESTIONS.length).fill(null));
+
+  const value = {
+    currentState,
+    setCurrentState,
+  };
   return (
     <div style={{ textAlign: "center" }}>
-      <React.Fragment></React.Fragment>
+      <Basic />
+      <Questionnaire answers={answers} setAnswers={setAnswers} />
+      <Optional />
     </div>
   );
-};
+}
 
 export default Confirm;
